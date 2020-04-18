f8066d5b3207027b1fb85fc5849c09f10c247feb
shun-shobon <shun.shobon@gmail.com>
2020-04-18 22:35:06 +0900

    chore: remove test

--- computes/main/jest.config.js
+++
@@ -1,6 +0,0 @@
-const config = require("../../jest.config")
-
-module.exports = {
-  ...config,
-  collectCoverageFrom: ["<rootDir>/src/**.ts", "!<rootDir>/src/app.ts"],
-}
--- computes/main/tests/hello.spec.ts
+++
@@ -1,8 +0,0 @@
-import hello from "../src/hello"
-
-describe("hello func", () => {
-  test("should be 'Hello, world!'", () => {
-    const name = "world"
-    expect(hello(name)).toBe("Hello, world!")
-  })
-})
