1755c934734c6344f4e3605f6b60e41f3cee3c52
shun-shobon <shun.shobon@gmail.com>
2020-04-19 21:39:12 +0900

    feat(main): add say grass func

---
+++ computes/main/src/grass/index.ts
@@ -0,0 +1,18 @@
+import * as database from "./database"
+import * as Discord from "discord.js"
+
+export const sayGrass = async (message: Discord.Message): Promise<void> => {
+  if (message.author.bot) return
+  if (!message.member?.id) return
+  if (Math.ceil(Math.random() * 100) % 5) return
+  if (!message.content.match(/(草|くさ|kusa)/iu)) return
+  const grass = await database.getGrass(message.member.id)
+  if (!grass?.enable) return
+  const count = grass[grass.target]
+  const grassPromises = "w"
+    .repeat(count)
+    .match(/.{1,2000}/g)
+    ?.map((grassStr) => message.channel.send(grassStr))
+  if (grassPromises) await Promise.all(grassPromises)
+  await message.channel.send(grass.image)
+}
