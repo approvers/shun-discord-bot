d0b058c90731b3bdacb19155dabb858578da13b1
shun-shobon <shun.shobon@gmail.com>
2020-04-19 15:01:11 +0900

    fix(functions): fix status code

--- functions/grass/src/getGrass.ts
+++ functions/grass/src/getGrass.ts
@@ -5,21 +5,17 @@ import * as storage from "./storage"
 module.exports = async (req: Request, res: Response) => {
   const name = req.body.name
   const isDark = req.body.dark
-  if (name == null || isDark == null)
-    return res.status(400).json({ success: false })
+  if (name == null || isDark == null) return res.status(400).send()
 
   const grass = await scraping.getData(name, isDark)
-  if (grass == null) return res.status(401).json({ success: false })
+  if (grass == null) return res.status(404).send()
 
   const image = await storage.addImage(name, grass.image)
 
   return res.status(200).json({
-    success: true,
-    data: {
-      today: grass.today,
-      week: grass.week,
-      year: grass.year,
-      image,
-    },
+    today: grass.today,
+    week: grass.week,
+    year: grass.year,
+    image,
   })
 }
