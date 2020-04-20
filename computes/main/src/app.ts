916a74a8adedebb41d96a868101612c709a1fa25
shun-shobon <shun.shobon@gmail.com>
2020-04-20 15:47:33 +0900

    feat(main): add discord

--- computes/main/src/app.ts
+++ computes/main/src/app.ts
@@ -1,3 +1,17 @@
-import hello from "./hello"
+import * as grass from "./grass"
+import * as Discord from "discord.js"
 
-console.log(hello("world"))
+const client = new Discord.Client()
+const token = process.env.DISCORD_TOKEN
+if (!token) throw new Error("env is not defined")
+
+client.login(token).catch(console.error)
+
+client.on("ready", () => console.log("Ready..."))
+
+client.on("message", async (message) => {
+  if (message.author.bot) return
+  await grass.sayGrass(message)
+  const commands = message.content.trim().replace(/\s+/g, " ").split(" ")
+  await grass.parseCommand(commands, message)
+})
