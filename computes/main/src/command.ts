27a97e7cb27177a2c4ab0323318ae0dc21e6c6ef
shun-shobon <shun.shobon@gmail.com>
2020-05-05 14:38:20 +0900

    refactor(main): refact command class

--- computes/main/src/command.ts
+++ computes/main/src/command.ts
@@ -1,17 +1,45 @@
 import { Message } from "discord.js"
 
-type CommandFunction = (args: string[], message: Message) => Promise<void>
+type CommandFunc = (args: string[], message: Message) => Promise<void>
+type CommandData = {
+  name: string
+  command: CommandFunc | Command
+}
 
-export default class {
-  private static commandFunctions: CommandFunction[] = []
-  static add(func: CommandFunction): void {
-    this.commandFunctions.push(func)
+export class Command {
+  private static globalCommands: CommandData[] = []
+
+  static add(name: string, command: CommandFunc | Command): void {
+    this.globalCommands.push({ name, command })
   }
-  static run(message: Message): Promise<void[]> {
-    const commands = message.content.trim().replace(/\s+/g, " ").split(" ")
-    const functionPromise = this.commandFunctions.map((func) =>
-      func(commands, message),
-    )
-    return Promise.all(functionPromise)
+
+  static async run(message: Message): Promise<void> {
+    const args = message.content.trim().replace(/\s+/g, " ").split(" ")
+    await Command.runCommands(args, message, this.globalCommands)
+  }
+
+  commands: CommandData[] = []
+
+  add(name: string, command: CommandFunc | Command): void {
+    this.commands.push({ name, command })
+  }
+
+  private async run(args: string[], message: Message): Promise<void> {
+    await Command.runCommands(args, message, this.commands)
+  }
+
+  private static async runCommands(
+    args: string[],
+    message: Message,
+    commands: CommandData[],
+  ): Promise<void> {
+    const runPromise = commands.map(({ name, command }) => {
+      if (args[0] !== name) return
+      if (command instanceof Command) {
+        return command.run(args.slice(1), message)
+      }
+      return command(args.slice(1), message)
+    })
+    await Promise.all(runPromise)
   }
 }
