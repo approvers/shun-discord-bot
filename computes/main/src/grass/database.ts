6a113ed4a801147c4c061ccf84baf8e7ccfb0bcc
shun-shobon <shun.shobon@gmail.com>
2020-05-05 10:39:19 +0900

    refactor(main): refact database.ts

--- computes/main/src/grass/database.ts
+++ computes/main/src/grass/database.ts
@@ -1,102 +1,101 @@
 import axios from "axios"
 import { Firestore } from "@google-cloud/firestore"
-import CollectionReference = FirebaseFirestore.CollectionReference
-import DocumentData = FirebaseFirestore.DocumentData
 
-interface Grass {
+export interface Grass {
   today: number
   week: number
   year: number
-  image: string
+  image: Buffer
 }
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const isGrass = (item: any): item is Grass =>
   typeof item?.today === "number" &&
   typeof item?.week === "number" &&
   typeof item?.year === "number" &&
-  typeof item?.image === "string"
+  item?.image instanceof Buffer
 
-interface Config {
-  enable?: boolean
-  dark?: boolean
-  display?: boolean
-  target?: "today" | "week" | "year"
-}
-interface GrassDoc extends Grass, Config {
-  name: string
+export interface Config {
   enable: boolean
   dark: boolean
   display: boolean
   target: "today" | "week" | "year"
 }
-const isGrassDoc = (item: any): item is GrassDoc =>
-  typeof item?.name === "string" &&
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const isConfig = (item: any): item is Config =>
   typeof item?.enable === "boolean" &&
   typeof item?.dark === "boolean" &&
   typeof item?.display === "boolean" &&
-  ["today", "week", "year"].includes(item?.target) &&
-  isGrass(item)
+  ["today", "week", "year"].includes(item?.target)
+
+interface GrassDoc extends Grass, Config {
+  name: string
+}
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const isGrassDoc = (item: any): item is GrassDoc =>
+  typeof item?.name === "string" && isGrass(item) && isConfig(item)
 
 const database = new Firestore()
-const grassCollection = (): CollectionReference<DocumentData> =>
-  database.collection("grass")
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+const grassCollection = () => database.collection("grass")
 
-export function fetchGrass(
+export const fetchGrass = async (
   name: string,
   isDark: boolean,
-): Promise<Grass | null> {
+): Promise<Grass | null> => {
   const url = process.env.GET_GRASS_URL
-  if (!url) throw new Error("env is undefined")
-  return axios
-    .post(url, { name, dark: isDark })
-    .then((res) => (isGrass(res.data) ? res.data : null))
-    .catch(() => null)
+  if (!url) throw new Error("GET_GRASS_URL is undefined")
+  const { data } = await axios.post(url, { name, dark: isDark })
+  if (!data?.image) return null
+  const image = Buffer.from(data.image, "base64")
+  const grass = { ...data, image }
+  return isGrass(grass) ? grass : null
 }
 
-export async function setGrass(userId: string, grass: GrassDoc): Promise<void> {
-  await grassCollection()
-    .doc(userId)
-    .set({
-      today: grass.today,
-      week: grass.week,
-      year: grass.year,
-      image: grass.image,
-      enable: grass.enable,
-      dark: grass.dark,
-      display: grass.display,
-      target: grass.target,
-      name: grass.name,
-    } as GrassDoc)
-}
-
-export async function getGrass(userId: string): Promise<GrassDoc | null> {
-  const ref = await grassCollection().doc(userId).get()
-  const grass = ref.data()
-  return isGrassDoc(grass) ? grass : null
-}
-
-export async function updateGrass(userId: string): Promise<void> {
-  const user = await getGrass(userId)
-  if (!user) throw new Error("user is not found")
-  const grass = await fetchGrass(user.name, user.dark)
-  if (!grass) return
-  await grassCollection().doc(userId).update(grass)
-}
-
-export async function updateAllGrass(): Promise<void> {
-  const docs = await grassCollection().get()
-  const updatePromise: Promise<void>[] = []
-  docs.forEach((doc) => {
-    const id = doc.id
-    updatePromise.push(updateGrass(id))
-  })
-  await Promise.all(updatePromise)
-}
-
-export async function updateConfig(
+export const setGrass = async (
   userId: string,
-  config: Config,
-): Promise<void> {
+  grass: GrassDoc,
+): Promise<void> => {
+  const data: GrassDoc = {
+    today: grass.today,
+    week: grass.week,
+    year: grass.year,
+    image: grass.image,
+    enable: grass.enable,
+    dark: grass.dark,
+    display: grass.display,
+    target: grass.target,
+    name: grass.name,
+  }
+  await grassCollection().doc(userId).set(data)
+}
+
+export const getGrass = async (userId: string): Promise<GrassDoc | null> => {
+  const ref = await grassCollection().doc(userId).get()
+  const data = ref.data()
+  return isGrassDoc(data) ? data : null
+}
+
+export const updateConfig = async (
+  userId: string,
+  config: Partial<Config>,
+): Promise<boolean> => {
   const user = await getGrass(userId)
-  if (!user) throw new Error("user is not found")
-  await grassCollection().doc(userId).update(config)
+  if (!user) return false
+  await grassCollection().doc().update(config)
+  return true
+}
+
+export const updateGrass = async (userId: string): Promise<boolean> => {
+  const user = await getGrass(userId)
+  if (!user) return false
+  const grass = await fetchGrass(user.name, user.dark)
+  if (!grass) return false
+  await grassCollection().doc(userId).update(grass)
+  return true
+}
+
+export const updateAllGrass = async (): Promise<void> => {
+  const { docs } = await grassCollection().get()
+  const updatePromise = docs.map(({ id }) => updateGrass(id))
+  await Promise.all(updatePromise)
 }
